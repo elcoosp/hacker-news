f944337d665041727358faec180a2764ef75dff2
Louis Cospain <elcoosp@gmail.com>
2018-10-02 23:10:40 +0200

    refactor: 💡 Store boilerplate

---
+++ src/redux/store.js
@@ -0,0 +1,19 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+
+/** Middlewares */
+import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import createSagaMiddleware from 'redux-saga'
+/** Reducers */
+import feed, { feedSagas } from './Feed'
+
+const sagaMiddleware = createSagaMiddleware()
+const rootReducer = combineReducers({ feed })
+
+const store = createStore(
+	rootReducer,
+	composeWithDevTools(applyMiddleware(sagaMiddleware, logger))
+)
+sagaMiddleware.run(feedSagas)
+
+export default store
