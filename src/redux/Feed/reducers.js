ee2dd26aab882c0c1862d68ff37d9c7f68536ad5
Louis Cospain <elcoosp@gmail.com>
2018-10-12 22:33:28 +0200

    refactor: 💡 Handle FETCH_FEED_RESPONSE inside reducer

--- src/redux/Feed/reducers.js
+++ src/redux/Feed/reducers.js
@@ -1,10 +1,20 @@
 import * as T from './types'
 
 const rootReducer = (
-	state = { meta: { isFetching: false, error: '' } },
+	state = {
+		meta: { isFetching: false, error: '' },
+		feeds: { new: {}, top: {}, best: {} }
+	},
 	{ type, payload }
 ) => {
 	switch (type) {
+		case T.FETCH_FEED_RESPONSE:
+			const { feedName, byIds, feed } = payload
+			return {
+				...state,
+				meta: { isFetching: false, error: '' },
+				feeds: { ...state.feeds, [feedName]: { byIds, feed } }
+			}
 		case T.FETCH_FEED_REQUEST:
 			return { ...state, meta: { isFetching: true } }
 		case T.FETCH_FEED_ERROR:
